5af6eee39f89ee047b3d81f0d3fd3f5b2ee1d9bf
Ashley Bland <ashleyjbland@gmail.com>
2019-02-28 23:52:34 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,16 +1,5 @@
 class CreateArtists < ActiveRecord::Migration
 
-   #def self.create_table
-  #  sql = <<-SQL
-  #    CREATE TABLE IF NOT EXISTS songs (
-  #    id INTEGER PRIMARY KEY,
-  #    title TEXT,
-  #    length INTEGER
-  #    )
-  #  SQL
-  #  ActiveRecord::Base.connection.execute(sql)
-  #end
-
    def change
     create_table :artists do |t|
       t.string :name
